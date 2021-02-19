1fe20173374e4f92eee6ce6f64c3b518cc83a378
yhchan0918 <yhchan0918@gmail.com>
2021-02-19 16:16:14 +0800

    implmenet backend amplify

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { withAuthenticator } from 'aws-amplify-react-native';
 
@@ -7,13 +7,46 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-import Amplify from 'aws-amplify';
+import Amplify, { Auth, API, graphqlOperation } from 'aws-amplify';
+import { getUser } from './src/graphql/queries';
+import { createUser } from './src/graphql/mutations';
 import config from './aws-exports';
 Amplify.configure(config);
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      // get authenticated user from Auth
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      if (userInfo) {
+        // get user from backend with the userId from Auth
+        const userData = await API.graphql(
+          graphqlOperation(getUser, { id: userInfo.attributes.sub })
+        );
+        if (userData.data.getUser) {
+          console.log('User is already existed in database');
+          return;
+        }
+        // If there is no user with the id,the create one
+        const newUser = {
+          id: userInfo.attributes.sub,
+          name: userInfo.username,
+          imageUri:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwMtEVoV1bCSZDno7eKQQw7xaodzngOTNNIQ&usqp=CAU',
+          status: 'Hey, I am using MessagesApp',
+        };
+        console.log(newUser);
+        await API.graphql(graphqlOperation(createUser, { input: newUser }));
+      }
+    };
+    fetchUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -1,12 +1,23 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
+import { API, graphqlOperation } from 'aws-amplify';
 
 import ContactListItem from '../components/ContactListItem';
-
 import { View } from '../components/Themed';
 import users from '../data/Users';
+import { listUsers } from '../src/graphql/queries';
 
 export default function ContactsScreen() {
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const usersData = await API.graphql(graphqlOperation(listUsers));
+        console.log(usersData);
+      } catch (e) {}
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <View style={styles.container}>
       <FlatList
