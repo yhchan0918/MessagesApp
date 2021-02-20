3c5bc8d58ea161eb143a34a4080922bb1ffc18b1
yhchan0918 <yhchan0918@gmail.com>
2021-02-20 18:36:22 +0800

    fix keyboard avoiding view

--- components/InputBox/index.tsx
+++ components/InputBox/index.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, TouchableOpacity } from 'react-native';
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 import {
   MaterialCommunityIcons,
   FontAwesome5,
@@ -64,31 +70,51 @@ const InputBox = (props) => {
     }
   };
   return (
-    <View style={styles.container}>
-      <View style={styles.mainContainer}>
-        <FontAwesome5 name="laugh-beam" size={24} color="grey" />
-        <TextInput
-          placeholder={'Type a message'}
-          style={styles.input}
-          multiline
-          value={msg}
-          onChangeText={(text) => setMsg(text)}
-        />
-        <Entypo name="attachment" size={24} color="grey" style={styles.icon} />
-        {!msg && (
-          <Fontisto name="camera" size={24} color="grey" style={styles.icon} />
-        )}
-      </View>
-      <TouchableOpacity onPress={onPress}>
-        <View style={styles.buttonContainer}>
-          {msg ? (
-            <MaterialIcons name="send" size={26} color="white" />
-          ) : (
-            <MaterialCommunityIcons name="microphone" size={28} color="white" />
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={85}
+      style={{ width: '100%' }}
+    >
+      <View style={styles.container}>
+        <View style={styles.mainContainer}>
+          <FontAwesome5 name="laugh-beam" size={24} color="grey" />
+          <TextInput
+            placeholder={'Type a message'}
+            style={styles.input}
+            multiline
+            value={msg}
+            onChangeText={(text) => setMsg(text)}
+          />
+          <Entypo
+            name="attachment"
+            size={24}
+            color="grey"
+            style={styles.icon}
+          />
+          {!msg && (
+            <Fontisto
+              name="camera"
+              size={24}
+              color="grey"
+              style={styles.icon}
+            />
           )}
         </View>
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity onPress={onPress}>
+          <View style={styles.buttonContainer}>
+            {msg ? (
+              <MaterialIcons name="send" size={26} color="white" />
+            ) : (
+              <MaterialCommunityIcons
+                name="microphone"
+                size={28}
+                color="white"
+              />
+            )}
+          </View>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
